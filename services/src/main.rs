ab865c6b32f153d9dedbc4c2366a10c0f8e3504c
Carlos Ramos <57056947+CarlosIRamosV@users.noreply.github.com>
2023-11-11 12:29:20 -0600

    test api

--- services/src/main.rs
+++ services/src/main.rs
@@ -1,26 +1,28 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, HttpRequest};
+use serde::{Deserialize, Serialize};
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+#[derive(Serialize)]
+struct Info {
+    name: String,
+}
+#[derive(Deserialize)]
+struct User {
+    name: String,
+    username: String,
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+#[post("/user")]
+async fn user(info: web::Json<User>) -> impl Responder {
+    HttpResponse::NotFound().json(Info {
+        name: info.name.clone(),
+    })
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            .service(user)
     })
     .bind(("0.0.0.0", 8080))?
     .run()
