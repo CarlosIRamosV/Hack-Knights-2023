262333097b19e4ec1e204df0d18630e50c2492c5
DiegoSandovalB <l21550307@chihuahua2.tecnm.mx>
2023-11-11 12:54:54 -0600

    Login e interfaz

---
+++ app/interfaz/interfaz.tsx
@@ -0,0 +1,16 @@
+export default function Login() {
+    return (
+      <main>
+        <div>
+
+<div class="navbar">
+    <a href="#"><i class="fas fa-home"></i> Inicio</a>
+    <a href="#"><i class="fas fa-envelope"></i> Mensajes</a>
+    <a href="#"><i class="fas fa-bell"></i> Notificaciones</a>
+    <a href="#"><i class="fas fa-cog"></i> Configuración</a>
+</div>
+       
+      </main>
+    )
+  }
+  
